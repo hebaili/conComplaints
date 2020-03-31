1c2a8a2ad7f43e5aba35a36a6a6a0ed8cfdd161e
baili He <bailihe@bailis-MBP.wavecable.com>
2020-03-30 21:08:27 -0700

    modify run.sh

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,17 @@ import java.io.IOException;
 
 public class Main {
 
+
     static String input = "complaints2.csv";
     //static String preprocessed;
 
 
     public static void main(String[] args) throws IOException {
 	// write your code here
+        for (String argument : args) {
+            // do something with the argument
+            System.out.println(argument);
+        }
         Preprocessor preprocessor = new Preprocessor();
         preprocessor.preprocess(input);
         Analyzer analyzer = new Analyzer();
